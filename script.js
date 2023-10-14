5716ae93498b2caa87cebeeaef1adf99b0c6cc47
Harry Pearson <harrypearson95@gmail.com>
2023-10-14 18:08:28 +0100

    intial site version

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+*/
+
+function openNav() {
+document.getElementById("myNav").style.height = "100vh";
+document.getElementById("myNav").style.width = "100vw";
+}
+
+function closeNav() {
+
+document.getElementById("myNav").style.height = "0%";
+}
